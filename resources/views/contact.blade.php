6d47bd392dffc981be3d133b1cceb359dd863629
Bowornrat <bowornrat.t@ku.th>
2025-12-20 15:12:34 +0700

    feat: assignment 4, part 2

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+
+@section('content')
+    <h1 class="text-4xl text-center text-blue-600">
+        Contact Us
+    </h1>
+    <h2 class="text-3xl text-center mt-4">
+        6610402132 บวรรัตน์ ตั้งนรารัชชกิจ
+    </h2>
+@endsection
---
+++ resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('content')
+    <h1 class='text-4xl text-center text-red-500'>
+        Welcome to Laravel Application
+    </h1>
+@endsection
