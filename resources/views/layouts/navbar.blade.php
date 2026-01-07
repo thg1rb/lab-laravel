b2d81024e34d57ad75187ba07a376b7cc14670fe
Bowornrat <bowornrat.t@ku.th>
2026-01-07 11:07:12 +0700

    feat: authentication and authorization

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,18 +1,25 @@
-<nav class="bg-gray-50 border-gray-200 py-2.5 dark:bg-gray-900">
+<nav class="bg-gray-50 border-gray-200 py-2.5">
     <div class="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
         <a href="/" class="flex items-center">
             <img src="https://www.svgrepo.com/show/416651/globe-world-earth.svg" class="h-10 mr-3 sm:h-20" alt="Landwind Logo">
-            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Web Tech.</span>
+            <span class="self-center text-xl font-semibold whitespace-nowrap">Web Tech.</span>
         </a>
         <div class="flex items-center lg:order-2">
             <div class="hidden mt-2 mr-4 sm:inline-block">
                 <span></span>
             </div>
 
-            <a href="{{ url('/contact') }}"
-                class="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800">Contact Me</a>
+            @guest
+            <div class"flex flex-row gap-x-3">
+                <a href="{{ url('/login') }}"
+                    class="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 focus:outline-none">Login</a>
+                <a href="{{ url('/register') }}"
+                    class="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 focus:outline-none">Register</a>
+            </div>
+            @endguest
+            @auth
             <button data-collapse-toggle="mobile-menu-2" type="button"
-				class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+				class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
 				aria-controls="mobile-menu-2" aria-expanded="true">
 				<span class="sr-only">Open main menu</span>
 				<svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -26,24 +33,27 @@
 						clip-rule="evenodd"></path>
 				</svg>
 			</button>
+			@endauth
         </div>
+        @auth
         <div class="items-center justify-between w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
             <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                 <li>
                     <a href="{{ url('/') }}"
-                        class="block py-2 pl-3 pr-4 {{ request()->is('/') ? 'text-white bg-purple-700 rounded lg:bg-transparent lg:text-purple-700' : 'text-gray-700 hover:bg-gray-50 lg:hover:bg-transparent lg:hover:text-purple-700' }} lg:p-0 dark:text-white"
+                        class="block py-2 pl-3 pr-4 {{ request()->is('/') ? 'text-white bg-purple-700 rounded lg:bg-transparent lg:text-purple-700' : 'text-gray-700 hover:bg-gray-50 lg:hover:bg-transparent lg:hover:text-purple-700' }} lg:p-0"
                         aria-current="page">Home</a>
                 </li>
                 <li>
                     <a href="{{ url('/students') }}"
-                        class="block py-2 pl-3 pr-4 {{ request()->is('students') ? 'text-white bg-purple-700 rounded lg:bg-transparent lg:text-purple-700' : 'text-gray-700 hover:bg-gray-50 lg:hover:bg-transparent lg:hover:text-purple-700' }} border-b border-gray-100 lg:border-0 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Students</a>
+                        class="block py-2 pl-3 pr-4 {{ request()->is('students') ? 'text-white bg-purple-700 rounded lg:bg-transparent lg:text-purple-700' : 'text-gray-700 hover:bg-gray-50 lg:hover:bg-transparent lg:hover:text-purple-700' }} border-b border-gray-100 lg:border-0 lg:p-0 lg:dark:hover:text-white lg:dark:hover:bg-transparent">Students</a>
                 </li>
                 <li>
                     <a href="{{ url('/professors') }}"
-                        class="block py-2 pl-3 pr-4 {{ request()->is('professors') ? 'text-white bg-purple-700 rounded lg:bg-transparent lg:text-purple-700' : 'text-gray-700 hover:bg-gray-50 lg:hover:bg-transparent lg:hover:text-purple-700' }} border-b border-gray-100 lg:border-0 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Professors</a>
+                        class="block py-2 pl-3 pr-4 {{ request()->is('professors') ? 'text-white bg-purple-700 rounded lg:bg-transparent lg:text-purple-700' : 'text-gray-700 hover:bg-gray-50 lg:hover:bg-transparent lg:hover:text-purple-700' }} border-b border-gray-100 lg:border-0 lg:p-0 lg:dark:hover:text-white lg:dark:hover:bg-transparent">Professors</a>
                 </li>
             </ul>
         </div>
+        @endauth
     </div>
 </nav>
 
--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,22 @@
 
 use App\Http\Controllers\ArtistController;
 use App\Http\Controllers\ProfessorsController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\StudentsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
+})->name('home');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 Route::get('/contact', function () {
@@ -20,3 +31,5 @@ Route::get('/professors', [ProfessorsController::class, 'index']);
 Route::get('/professors/{id}', [ProfessorsController::class, 'show']);
 
 Route::resource('artists', ArtistController::class);
+
+require __DIR__.'/auth.php';
